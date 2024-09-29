d222ff4b97cde458e81ea13655a66c5b39233fc1
mainak55512 <mbhattacharjee432@gmail.com>
2024-09-29 07:47:44 +0530

    --sort added

--- process/process_flags.go
+++ process/process_flags.go
@@ -2,6 +2,7 @@ package process
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 
 	"github.com/mainak55512/stto/utils"
@@ -37,6 +38,11 @@ func ProcessByFlags(file_details *[]utils.File_details, folder_name *string, is_
 		err := ProcessConcurrentWorkers(file_details, folder_count, folder_name, is_git_initialized, mu, wg)
 		if err != nil {
 			fmt.Println(fmt.Errorf("%w", err))
+		}
+		if *inpFlags.Sort == true {
+			sort.Slice(*file_details, func(i, j int) bool {
+				return (*file_details)[i].Line_count > (*file_details)[j].Line_count
+			})
 		}
 		err = utils.EmitTable(inpFlags.Lang, file_details, folder_name, is_git_initialized, folder_count)
 		if err != nil {
